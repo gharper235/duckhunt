4527dafc163f9af8ca16d42263d60e6d2355fc14
Gregory Harper <gharper235@gmail.com>
2021-01-02 16:42:25 -0600

    css hide for small browsers

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function player1Turn(){
+    window.alert("It is Player 1's turn");
+}
+
+function player2Turn(){
+    window.alert("It is Player 2's turn");
+}
+
+function help() {
+    window.alert("Shoot the ducks by clicking directly on them to gain a point. The player with the most points at end of 3 rounds wins, if it's not a tie!");
+}
